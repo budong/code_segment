17dc635d21e910c410b094e1911ae49967b74077
peiqiang <sias_peiqiang@163.com>
2013-11-13 21:39:26 +0800

    variable

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char firstletter = 'a';
+int hour = 1;
+int minute = 15;
+
+/*有些问题，分开就不行了，先留个坑*/
+
+
+int main(void)
+{
+    printf("firstletter: %c\nhour: %d\nminute: %d\n",firstletter,hour,minute);
+}
