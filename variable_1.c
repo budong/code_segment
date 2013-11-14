cbadf02a8bc07a4e3cf30ac44ad9ce3b75c5efc7
peiqiang <sias_peiqiang@163.com>
2013-11-14 23:34:58 +0800

    variable

---
+++ variable_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char firstletter;
+int hour = 1;
+int minute = 15;
+
+//firstletter = 'a';
+//hour = 2;
+
+int main(void)
+{
+    printf("firstletter: %c\nhour: %d\nminute: %d\n",firstletter,hour,minute);
+}
---
+++ variable_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char firstletter;
+int hour = 1;
+int minute = 15;
+
+
+int main(void)
+{
+    printf("firstletter: %c\nhour: %d\nminute: %d\n",firstletter,hour,minute);
+    printf("hour + minute = %d\n",minute + hour);
+}
