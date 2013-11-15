ff1bd24ded5d436b4e5fdfc106ba3e4bc680907b
peiqiang <sias_peiqiang@163.com>
2013-11-15 21:25:18 +0800

    全局变量

---
+++ variable_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char firstletter;
+int hour = 1;
+int minute = 15;
+int sum;
+
+
+int main(void)
+{
+    sum = hour + minute;
+    //printf("firstletter: %c\nhour: %d\nminute: %d\n",firstletter,hour,minute);
+    //printf("hour + minute = %d\n",minute + hour);
+    printf("sum: %d\n",sum);
+}
