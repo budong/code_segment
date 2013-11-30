c66d13dc834f12db9a9ddc51c334baad866ddbcb
peiqiang <sias_peiqiang@163.com>
2013-11-30 22:49:17 +0800

    if

---
+++ if_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int x = -1;
+int main(void)
+{
+if (x > 0)
+    printf("x is a positive.\n");
+else if(x < 0)
+    printf("x is a negative.\n");
+else
+    printf("x is a zero\n");
+}
