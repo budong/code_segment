54e04bd3579dae598acbad50417ba1f3cb6e7e05
peiqiang <sias_peiqiang@163.com>
2013-12-03 21:00:25 +0800

    factorial_3

---
+++ factorial_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int factorial(int n)
+{
+    int result = 1;
+    int i;
+    for(i = 1;i <=n;i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int main(void)
+{
+    printf("%d\n",factorial(4));
+}
