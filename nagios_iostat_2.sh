ae4cabd869a195f50f3f07ae59d2fa5c492c0cd9
peiqiang <sias_peiqiang@163.com>
2013-12-19 18:44:13 +0800

    nagios_iostat

--- nagios_iostat_2.sh
+++ nagios_iostat_2.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-#http://www.361way.com/nagios-check_io/1687.html
 iostat=`which iostat 2>/dev/null`
 bc=`which bc 2>/dev/null`
 function help {
-echo -e "This plugin shows the I/O usage_rate of the specified disk, using the iostat external program.nt example nt ./io -d sda2 -w 10 -c 20"
+echo -e "This plugin shows the I/O usage_rate of the specified disk, using the iostat external program.nt example nt ./io  -w 10 -c 20"
         exit -1
 }
 # Ensuring we have the needed tools:
 ( [ ! -f $iostat ] ) && ( echo "ERROR: iostat command not found .Please install" && exit -1 )
 # Getting parameters:
-while getopts "d:w:c:h" OPT; do
+while getopts "w:c:h" OPT; do
         case $OPT in
-                "d") disk=$OPTARG;;
                 "w") warning=$OPTARG;;
                 "c") critical=$OPTARG;;
                 "h") help;;
@@ -21,11 +19,14 @@ done
 crit_util=`echo $critical`
 warn_util=`echo $warning`
 # Checking parameters:
-[ ! -b "/dev/$disk" ] && echo "ERROR: Device incorrectly specified" && help
+#[ ! -b "/dev/$disk" ] && echo "ERROR: Device incorrectly specified" && help
 ( [ $warn_util == "" ] || [ $crit_util == "" ] ) && echo "ERROR: You must specify all warning and critical levels" && help
 ( [[ "$warn_util" -ge  "$crit_util" ]] ) && echo "ERROR: critical levels must be highter than warning levels" && help
 # Doing the actual check:
-util=`$iostat -dx 1 10 $disk | grep $disk | awk '{print $12}'|sort -nr | head -n 1 `
+#getio=`$iostat -dx 1 3 -p ALL| awk '{print $NF,$1}'|grep sda|sort -nr|head -n 1`
+getio=`$iostat -dx 1 3| awk '{print $NF,$1}'|grep sda|sort -nr|head -n 1`
+util=`echo $getio|awk '{ print $1}'`
+getdisk=`echo $getio|awk '{print $2}' `
 # Comparing the result and setting the correct level:
 if ( echo ${util} ${crit_util}|awk '!($1>=$2){exit 1}' );then
         msg="CRITICAL"
@@ -39,6 +40,6 @@ else if ( echo ${util} ${warn_util} |awk '!($1>=$2){exit 1}');then
      fi
 fi
 # Printing the results:
-echo "$msg - I/O stats util_rate=$util  "
+echo "$msg - $getdisk I/O stats util_rate=$util  "
 # Bye!
 exit $status
