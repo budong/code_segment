186f80224b92fd4771a954435b4513e8be9eb0a0
peiqiang <sias_peiqiang@163.com>
2013-12-29 21:56:04 +0800

    translator

--- python_cookbook.py
+++ python_cookbook.py
@@ -20,3 +20,39 @@ a
 
 In [15]: print str(97)
 97
+
+2.反转字符串
+In [38]: my
+Out[38]: 'I am lee'
+
+In [39]: ''.join(reversed(re.split(r'(\s)',my)))
+Out[39]: 'lee am I'
+
+In [40]: my[::-1]
+Out[40]: 'eel ma I'
+
+
+3.string.maketrans string.maketrans
+In [1]: import string
+
+In [2]: help(string.maketrans)
+
+
+In [3]: s = 'abcdefg-1234567'
+
+In [4]: table=string.maketrans("","")
+
+In [5]: s.translate(table)
+Out[5]: 'abcdefg-1234567'
+
+In [6]: s.translate(table,'abc123')
+Out[6]: 'defg-4567'
+
+In [7]: table=string.maketrans('abc','ABC')
+
+In [8]: s.translate(table)
+Out[8]: 'ABCdefg-1234567'
+
+In [9]: s.translate(table,'ab123')
+Out[9]: 'Cdefg-4567'
+
