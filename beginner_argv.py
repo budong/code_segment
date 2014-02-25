677334c12b4c62eb8db8af4286fff785edb3d579
peiqiang <sias_peiqiang@163.com>
2014-02-25 11:12:25 +0800

    sys.argv

---
+++ beginner_argv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+#coding: utf-8
+
+import sys
+
+print "This is the name of the script: ", sys.argv[0]
+print "Number of argument: ", len(sys.argv)
+print "The arguments are: ", str(sys.argv)
+print sys.argv
