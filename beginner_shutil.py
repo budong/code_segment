4eda72771a549fa89fefb00e88ccbd4b2202c387
peiqiang <sias_peiqiang@163.com>
2014-02-26 21:43:39 +0800

    shutil

---
+++ beginner_shutil.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2.7
+#coding: utf-8
+
+import shutil
+import os
+
+#source = os.listdir("/root")
+#destination = "/tmp/newfoler"
+#
+#for files in source:
+#    if files.endswith(".log"):
+#        shutil.copy("%s" % os.path.join("/root",files),destination)
+
+#shutil.copyfile("/root/install.log","/tmp/budong.log")
+
+
+#source = os.listdir("/root")
+#destination = "/tmp/newfoler"
+#
+#for files in source:
+#    if files.endswith(".log"):
+#        shutil.move("%s" % os.path.join("/root",files),destination)
+
+#shutil.copytree("/root/fabric","/tmp/fab")
+
+#shutil.rmtree("/tmp/newfoler")
